fb7113c54af2ecf6726add3061b474824574a3de
Lucas Moraes <lucasmoraes2@outlook.com.br>
2020-06-24 21:27:30 -0300

    Update

--- src/app.js
+++ src/app.js
@@ -46,7 +46,7 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
 
-  const repositoryIndex = repositories.find(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(400).json({ error: 'Repository not found.' });
